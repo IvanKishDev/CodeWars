c216cd7536ce7e83e6be60c90e4d5406d7b7ca22
IvanKishDev <aphrodisiak7@gmail.com>
2026-09-04 00:09:14 +0300

    complete new task

---
+++ capital.js
@@ -0,0 +1,30 @@
+// Complete the method that takes a sequence of objects with two keys each: country or state, and capital. Keys may be symbols or strings.
+//
+//     The method should return an array of sentences declaring the state or country and its capital.
+//
+//     Examples
+// state_capitals = [{state: 'Maine', capital: 'Augusta'}]
+// capital(state_capitals)[0] // returns "The capital of Maine is Augusta"
+//
+// country_capitals = [{'country' : 'Spain', 'capital' : 'Madrid'}]
+// capital(country_capitals)[0]  // returns "The capital of Spain is Madrid"
+//
+// mixed_capitals: [{"state" : 'Maine', capital: 'Augusta'}, {country: 'Spain', "capital" : "Madrid"}]
+// capital(mixed_capitals)[1] // returns "The capital of Spain is Madrid"
+
+
+
+
+
+function capital(capitals) {
+    let result = []
+
+
+    capitals.forEach(obj => {
+        const name = obj.state || obj.country
+
+        const capitalCity = obj.capital
+        result.push(`The capital of ${name} is ${capitalCity}`)
+    })
+    return result
+}
