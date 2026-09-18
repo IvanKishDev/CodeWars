85a3326d9f1fbc32a5c8cb385bfcaa7013582b70
IvanKishDev <aphrodisiak7@gmail.com>
2026-09-18 22:16:09 +0300

    complete multiples task

---
+++ multiples.js
@@ -0,0 +1,20 @@
+// Given 3 positive parameters a, b, limit, return all positive numbers that are a multiple of both a and b up to and including limit.
+//
+//     Examples
+// 1, 5, 15 --> [5, 10, 15]
+// 3, 5, 15 --> [15]
+// 3, 5, 40 --> [15, 30]
+// 2, 4, 40 --> [4, 8, 12, 16, 20, 24, 28, 32, 36, 40]
+
+
+function multiples(a, b, limit) {
+    let arr = []
+    let number = 0;
+
+    for (let i = 1; i <= limit; i++) {
+        if (i%a === 0 && i%b === 0) {
+            arr.push(i)
+        }
+    }
+    return arr;
+}
